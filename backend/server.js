8e2d3487fbb669e550216bd6b9e5a5e96e8ff7cd
unknown <moussaabdillahi542@gmail.com>
2024-09-20 06:28:40 +0300

    corrected for all

--- backend/server.js
+++ backend/server.js
@@ -18,13 +18,14 @@ app.use('/doctor',require('./View/doctors/index.js'))
 app.use('/users',require('./View/users/index.js'))
 
 if(process.env.NODE_ENV ==="production"){
-app.use(express.static(path.join(__dirname,'/frontend/dist')))
+app.use(express.static(path.join(__dirname,'../frontend/dist')))
 app.get('*',(req,res)=>{
   
-  res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+  res.sendFile(path.resolve("frontend","dist","index.html"))
 })
 
 }
+
 
 
 
@@ -39,7 +40,7 @@ app.get('*',(req,res)=>{
 
 
 
+    app.listen(process.env.PORT || 9800,()=>'listening server in port 8000')
 
 
 
-app.listen(process.env.PORT || 9800,()=>'listening server in port 8000')
