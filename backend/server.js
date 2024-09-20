584eef1193223f6fa795a675a9c3bc3717414893
unknown <moussaabdillahi542@gmail.com>
2024-09-20 06:04:22 +0300

    corrected

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,9 @@ app.use('/doctor',require('./View/doctors/index.js'))
 app.use('/users',require('./View/users/index.js'))
 
 if(process.env.NODE_ENV ==="production"){
-app.use(express.static(path.join(__dirname,'../frontend/dist')))
+app.use(express.static(path.join(__dirname,'/frontend/dist')))
 app.get('*',(req,res)=>{
+  
   res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
 })
 
